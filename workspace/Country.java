dfea91a7e004e130cd391607ca1cb86efc827a20
XenonLemon <pd922503@gmail.com>
2026-01-12 19:28:30 +0000

    Commit 2 1/12/26

--- workspace/Country.java
+++ workspace/Country.java
@@ -15,6 +15,18 @@ public class Country
 
   }
   // Write accessor/get methods for each instance variable that returns it.
+  public String getCountry(){
+    return name;
+  }
+  public String getCapital(){
+    return capital;
+  }
+  public String getLanguage(){
+    return language;
+  }
+  public String getImage(){
+    return image;
+  }
 
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
  
